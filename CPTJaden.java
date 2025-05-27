e6cc64886ab79e92242e24262e73f9d6fb753368
Jaden Lau <145374639+Puffy99@users.noreply.github.com>
2025-05-27 10:17:42 -0400

    Update CPTJaden.java

--- CPTJaden.java
+++ CPTJaden.java
@@ -4,6 +4,22 @@ public class CPTJaden {
 	
 	public static void main (String[] args) {
 		Console con = new Console();
-		con.println("Hello");
-	}
+
+        // Create a 6x7 board
+        int[][] board = new int[6][7];
+        con.println("Connect 4");
+        
+        // Print column numbers
+        for (int intCol = 1; intCol <= 7; intCol++) {
+            con.print(" " + intCol + " ");
+        }
+        con.println();
+        
+        for (int intRow = 0; intRow < 6; intRow++) {
+            for (int intCol = 0; intCol < 7; intCol++) {
+                con.print("[ ]");
+            }
+            con.println();
+        }
+    }
 }
